256ac72f0856ab20122e86c718104552fe345258
tagoma <23656943+tagomatech@users.noreply.github.com>
2020-05-14 16:30:59 +0200

    Update nbs.py

--- nbs/nbs.py
+++ nbs/nbs.py
@@ -7,7 +7,7 @@ import numpy as np
 class NBS_Fetcher(object):
     """Returns data from China's National Bureau of Statistics (NBS)."""
     
-     def __init__(self, database, product, geo='', flow='01', period='LAST13'):
+     def __init__(self, database, product, geo='', measure='01', period='LAST13'):
         """
         
         Parameters
@@ -26,42 +26,36 @@ class NBS_Fetcher(object):
             international data, yearly      :   'gjnd'
             international data, monthly     :   'gjyd'
             3 main countries data, monthly  :   'gjydsdj'
-
             TODO: Only monthly data at national and province level where dealt with.
             Extend code to other geographical and time granularities
             
         product : str
             Crude oil (production or processing) or oil product (output). Possible values:
-
-            coal                    :	"A030101"
-            crude oil 	            :	"A030102"
-            natural gas	            :	"A030103"
-            coalbed gas             :	"A030104"
-            lng                     :	"A030105"
-            crude oil processing    :	"A030106" # "Processing Volume of Crude oil" => Runs? Throughput?
-            gasoline                :	"A030107"
-            kerosene                :	"A030108"
-            diesel oil              :	"A030109"
-            fuel oil                :	"A03010A"
-            naphtha                 :	"A03010B"
-            lpg                     :	"A03010C"
-            petroleum coke          :	"A03010D"
-            asphalt                 :	"A03010E"
-            coke                    :	"A03010F"
-            electricity             :	"A03010G"
-            thermal power           :	"A03010H"
-            hydro-electric power    :	"A03010I"
-            nuclear power           :	"A03010J"
-            wind power              :	"A03010K"
-            solar power             :	"A03010L"
-            gas	                    : "A03010M"
-
-
+            coal                    :   "A030101"
+            crude oil               :   "A030102"
+            natural gas             :   "A030103"
+            coalbed gas             :   "A030104"
+            lng                     :   "A030105"
+            crude oil processing    :   "A030106" # "Processing Volume of Crude oil" => Runs? Throughput?
+            gasoline                :   "A030107"
+            kerosene                :   "A030108"
+            diesel oil              :   "A030109"
+            fuel oil                :   "A03010A"
+            naphtha                 :   "A03010B"
+            lpg                     :   "A03010C"
+            petroleum coke          :   "A03010D"
+            asphalt                 :   "A03010E"
+            coke                    :   "A03010F"
+            electricity             :   "A03010G"
+            thermal power           :   "A03010H"
+            hydro-electric power    :   "A03010I"
+            nuclear power           :   "A03010J"
+            wind power              :   "A03010K"
+            solar power             :   "A03010L"
+            gas                     : "A03010M"
         geo : str, optional
             NBS geographical zone to fetch data for. Possible values:
-
             Provinces:
-
                 Beijing         :   "110000"
                 Tianjin         :   "120000"
                 Hebei           :   "130000"
@@ -93,66 +87,51 @@ class NBS_Fetcher(object):
                 Qinghai         :   "630000"
                 Ningxia         :   "640000"
                 Xinjiang        :   "650000"
-
             TODO: complete the list above for other geographical levels
-
-
-        flow : str
+        measure : str
             Data type required. Possible values:
-
             Current Period                              :   "01"
             Accumulated                                 :   "02"
             Growth Rate (The same period last year=100) :   "03"
             Accumulated Growth Rate(%)                  :   "04"
-
             TODO: check data type are always those in the list above across products
-
-
         period : str
             Timestamp or time range. Includes possible values below:
-
             13 most recent months   : "LAST13"
             24 most recent months   : "LAST24"
             36 most recent months   : "LAST36"
             Specifc year            : "2014", "2015", etc...
             Specific time range     : "2013-2015", "201303-2015-12"
             etc...
-
             TODO: Review the part of the code that creates the np.Series() object as
             it is likely to break when only 1 data point is returned
             
-
         Returns
         -------
         series
             The time series containing the required data
-
-
         Examples
         --------
         # Example 1 : China gasoline production, monthly volumes (10000 tons) by month from Jun-18 to May-19
         nbs = NBS_Fetcher('hgyd',
                             'A030107',
-                            flow='01',
+                            measure='01',
                             period='201806-201905')
         data = nbs.get_data()
-
         # Example 2 : Shandong crude oil processing, monthly growth rate for the past 13 months
         nbs = NBS_Fetcher('hgyd',
                             'A030106',
                             geo='310000',
-                            flow='03',
+                            measure='03',
                             period='LAST13')
         data = nbs.get_data()
-
-
         
          """
         
         self.database = database
         self.product = product
         self.geo = geo
-        self.flow = flow
+        self.measure = measure
         self.period = period
         
         # Structure of json returned from NBS server differ depending on the source database
@@ -166,7 +145,7 @@ class NBS_Fetcher(object):
         self.url_getOtherWds = '{}?m=getOtherWds&dbcode={}&rowcode=zb&colcode=sj&wds=[{{"wdcode":"zb","valuecode":"{}{}"}}]'.format(url_root,
                                                                                                                                     self.database,
                                                                                                                                     self.product,
-                                                                                                                                    self.flow)
+                                                                                                                                    self.measure)
         self.url_QueryData = '{}?m=QueryData&dbcode={}&rowcode=zb&colcode=sj&wds=[{{"wdcode":"reg","valuecode":"{}"}}]&dfwds=[{{"wdcode":"sj","valuecode":"{}"}}]'.format(url_root,
                                                                                                                                                                         self.database,
                                                                                                                                                                         self.geo,
